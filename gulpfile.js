478556832d49720019d5140b083e344aefacb85b
fbk-rena <fbk.rena@gmail.com>
2017-07-17 16:33:39 -0500

    Html movido, Css movido y watch funcionando

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+const gulp = require('gulp');
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass');
+const obfuscate = require('gulp-obfuscate');
+const browserSync = require('browser-sync').create();
+
+const rutas = {
+    js: "./src/js/*.js",
+    scss: "./src/scss/main.scss",
+    html: "./src/*.html",
+};
+gulp.task('moverHtml', () => {
+    gulp.src(rutas.html)
+    .pipe( gulp.dest("public"));  
+});
+
+/*gulp.task('moverJS', () => {
+    gulp.src(rutas.js)
+        .pipe(uglify())
+        .pipe(obfuscate())
+    gulp.dest("./public/js");
+});*/
+
+gulp.task('prepararCss', () => {
+    gulp.src(rutas.scss)
+        .pipe(sass({outputStyle: "compressed"})
+            .on('error', sass.logError))
+    .pipe(gulp.dest("public/css/"))
+});
+
+gulp.task('watchCss', () => {
+    browserSync.init({
+        //crea servidor y da la ruta donde buscar el index
+        server: {
+            baseDir: "./public"
+        }
+    });
+    //vigila los cambios, tiene dos parametros la ruta que vigila y la tarea que ejecuta cuando exista algun cambio en esa ruta
+    gulp.watch(rutas.scss, ['sass-watch'])
+});
+//
+gulp.task('sass-watch', ['prepararCss'], () => {
+    //recarga el servidor 
+    browserSync.reload();
+})
